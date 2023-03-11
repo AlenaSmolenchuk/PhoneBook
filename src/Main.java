505e695cea4dd80ce16f2830841bed953ae205f7
Alena Smolenchuk <124808324+AlenaSmolenchuk@users.noreply.github.com>
2023-03-11 19:58:34 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,6 @@
-import java.io.File;
-import java.util.List;
-
-
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Start searching...");
-
-        File directory = new File("directory.txt");
-        File find = new File("find.txt");
-
-        List findList = new Files().addFind(find);
-        List directoryList = new Files().addDir(directory);
-
-        Search.searching(findList,directoryList);
+        new Search().start();
     }
 
 }
