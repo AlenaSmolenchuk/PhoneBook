3798fbf712da524378196d5dbd5ba676dbe3ba0c
Alena <124808324+AlenaSmolenchuk@users.noreply.github.com>
2023-03-22 19:37:36 +0300

    Create Person.java

---
+++ src/Person.java
@@ -0,0 +1,33 @@
+public class Person {
+    private String name;
+    private String number;
+
+    public Person(String name, String number) {
+        this.name = name;
+        this.number = number;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", number='" + number + '\'' +
+                '}';
+    }
+}
