a258464c566aec85f0a808f772897eff9d0bb766
Alena <124808324+AlenaSmolenchuk@users.noreply.github.com>
2023-03-22 19:36:14 +0300

    Update and rename Timer.java to Time.java

---
+++ src/Time.java
@@ -0,0 +1,106 @@
+import java.time.Duration;
+
+public class Time {
+    private static long startSearchingTime;
+    private static long endSearchingTime;
+    private static long startSortingTime;
+    private static long endSortingTime;
+    private static long allTime;
+    private static long linearSearchTime;
+    private Duration duration;
+    private long minutes;
+    private long seconds;
+    private long milliseconds;
+
+    void startSearch() {
+        startSearchingTime = System.currentTimeMillis();
+
+    }
+
+    void endSearchTime() {
+        endSearchingTime = System.currentTimeMillis();
+
+    }
+
+    void startSorting() {
+        startSortingTime = System.currentTimeMillis();
+
+    }
+
+    long getAllSearchingTime() {
+        return endSearchingTime - startSearchingTime;
+    }
+
+    void endSorting() {
+        endSortingTime = System.currentTimeMillis();
+
+    }
+
+    long currentSortingTime() {
+        return System.currentTimeMillis() - startSortingTime;
+    }
+
+    long getAllSortingTime() {
+        return endSortingTime - startSortingTime;
+    }
+
+
+    public void setAllTime() {
+        allTime = getAllSortingTime() + getAllSearchingTime();
+    }
+
+    public void setLinearSearchTime() {
+        linearSearchTime = endSearchingTime - startSearchingTime;
+    }
+
+    public long getLinearSearchTime() {
+        return linearSearchTime;
+    }
+
+    void printAllTime() {
+        setAllTime();
+        duration = Duration.ofMillis(allTime);
+        seconds = duration.toSecondsPart();
+        minutes = duration.toMinutesPart();
+        milliseconds = duration.toMillisPart();
+        System.out.printf("Time taken: %d min. %d sec. %d ms.\n", minutes, seconds, milliseconds);
+    }
+
+    void printSearchingTime() {
+        duration = Duration.ofMillis(getAllSearchingTime());
+        seconds = duration.toSecondsPart();
+        minutes = duration.toMinutesPart();
+        milliseconds = duration.toMillisPart();
+        System.out.printf("\nSearching time: %d min. %d sec. %d ms.\n", minutes, seconds, milliseconds);
+    }
+
+    void printSortingTime() {
+        duration = Duration.ofMillis(endSortingTime - startSortingTime);
+        seconds = duration.toSecondsPart();
+        minutes = duration.toMinutesPart();
+        milliseconds = duration.toMillisPart();
+        System.out.printf("Sorting time: %d min. %d sec. %d ms.", minutes, seconds, milliseconds);
+    }
+
+    void printLinearSearchAndBubbleSortTime() {
+        duration = Duration.ofMillis(getAllSortingTime() + getLinearSearchTime());
+        seconds = duration.toSecondsPart();
+        minutes = duration.toMinutesPart();
+        milliseconds = duration.toMillisPart();
+        System.out.printf("Time taken: %d min. %d sec. %d ms.\n", minutes, seconds, milliseconds);
+
+    }
+
+    void printLinearSearchTime() {
+        duration = Duration.ofMillis(getLinearSearchTime());
+        seconds = duration.toSecondsPart();
+        minutes = duration.toMinutesPart();
+        milliseconds = duration.toMillisPart();
+        System.out.printf("Time taken: %d min. %d sec. %d ms.\n", minutes, seconds, milliseconds);
+    }
+
+
+    void printFoundEntries(int count, int namesCount) {
+        System.out.printf("Found %d / %d entries. ", count, namesCount);
+    }
+}
