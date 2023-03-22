95d7b55ea33d16b36b7efd1389f3e52fe0730fc0
Alena <124808324+AlenaSmolenchuk@users.noreply.github.com>
2023-03-22 19:33:57 +0300

    Update Files.java

--- src/Files.java
+++ src/Files.java
@@ -1,11 +1,13 @@
+
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 import java.util.*;
 
 class Files {
-   public static List<String> addFind(File find) {
-       List<String> findList = new ArrayList<>();
+
+    static List<String> addFind(File find) {
+        List<String> findList = new ArrayList<>();
        try (BufferedReader br = new BufferedReader(
                new java.io.FileReader(find))) {
            String line = null;
@@ -16,23 +18,24 @@ class Files {
            e.printStackTrace();
            System.out.println("No file found: " + find);
        }
-       return  findList;
+     return  findList;
    }
 
-   public static TreeMap<String,String> addDir(File directory) {
-      TreeMap<String,String> directoryList = new TreeMap<>();
+   static List<Person> addDir(File directory) {
+       List<Person> directoryList = new ArrayList<>();
        try (BufferedReader br = new BufferedReader(
                new java.io.FileReader(directory))) {
            String line = null;
            while ((line = br.readLine()) != null) {
-               String[] data = line.trim().split("(?<=\\d)");
-               directoryList.put(data[1], data[0]);
+               String number = String.valueOf(br.read());
+               String name = br.readLine();
+               directoryList.add(new Person(name.trim(), number));
            }
        } catch (IOException e) {
            e.printStackTrace();
            System.out.println("No file found: " + directory);
        }
-       return  directoryList;
+     return  directoryList;
    }
 }
 
