c471d5089fd8255c901f4f59d1644dd6e22637a5
Alena <124808324+AlenaSmolenchuk@users.noreply.github.com>
2023-04-09 19:07:29 +0300

    Update Sort.java

--- src/phonebook/Sort.java
+++ src/phonebook/Sort.java
@@ -1,19 +1,14 @@
 package phonebook;
 
-
-import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Sort {
-    private List<Person> directoryList;
-    private List<String> findList;
 
     Time time = new Time();
     Search search = new Search();
 
-
-    private void bubbleSort(List<Person> directoryList, List<String> findList) {
+    protected List<Person> bubbleSort(List<Person> directoryList, List<String> findList) {
         time.startSorting();
         for (int i = 0; i < directoryList.size() - 1; i++) {
             for (int j = 0; j < directoryList.size() - i - 1; j++) {
@@ -25,7 +20,7 @@ public class Sort {
                     time.printSortingTime();
                     System.out.print(" - STOPPED, moved to linear search");
                     time.printSearchingTime();
-                    return;
+                    return directoryList;
                 }
 
                 if (directoryList.get(j).getName().compareTo(directoryList.get(j + 1).getName()) > 0) {
@@ -36,23 +31,25 @@ public class Sort {
             }
         }
         time.endSorting();
-        search.jumpSearch(directoryList, findList);
+        return directoryList;
     }
 
-    private List<Person> quickSort(List<Person> directoryList) {
+     protected List<Person> quickSort(List<Person> directoryList) {
+         time.startSorting();
         if(directoryList.isEmpty()){
             return directoryList;
         }
-        time.startSorting();
         int pivot = directoryList.size() / 2;
-        int size = directoryList.size();
+
+        List<Person> sorted;
         List<Person> lesser = new ArrayList<>();
         List<Person> greater = new ArrayList<>();
+
         Person piv = directoryList.get(pivot);
         String pivotVal = piv.getName();
-        for (int i = 1; i < size; i++) {
-            Person p = directoryList.get(i);
 
+        for (int i = 1; i < directoryList.size(); i++) {
+            Person p = directoryList.get(i);
             if (p.getName().compareTo(pivotVal) < 0) {
                 lesser.add(p);
             } else {
@@ -64,24 +61,8 @@ public class Sort {
         greater = quickSort(greater);
         lesser.add(piv);
         lesser.addAll(greater);
+        sorted = lesser;
         time.endSorting();
-        return lesser;
-    }
-
-    public void start() {
-        directoryList = Files.addDir(new File("C:\\Users\\helen\\Phone Book (Java)\\directory.txt"));
-        findList = Files.addFind(new File("C:\\Users\\helen\\Phone Book (Java)\\find.txt"));
-
-        System.out.println("Start searching (linear search)...");
-        search.linearSearch(directoryList, findList, true);
-
-        Sort sort = new Sort();
-        System.out.println("Start searching (bubble sort + jump search)...");
-        sort.bubbleSort(directoryList, findList);
-
-        System.out.println("Start searching (quick sort + binary search)...");
-        List<Person> lesser = quickSort(directoryList);
-        search.binarySearch(lesser, findList);
-
+        return sorted;
     }
 }
