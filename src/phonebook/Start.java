351aa10db00f7df5e00b080163df6eb7b3f429cc
Alena <124808324+AlenaSmolenchuk@users.noreply.github.com>
2023-04-11 15:54:41 +0300

    Update Start.java

--- src/phonebook/Start.java
+++ src/phonebook/Start.java
@@ -1,1 +1,35 @@
-kdjljsl
+package phonebook;
+
+import java.io.File;
+import java.util.Hashtable;
+import java.util.List;
+
+public class Start {
+    private List<Person> directoryList;
+    private List<String> findList;
+    private Hashtable<String,String> directoryTable;
+
+    Search search = new Search();
+    Sort sort = new Sort();
+
+    public void start() {
+        directoryList = Files.addDir(new File("directory.txt"));
+        findList = Files.addFind(new File("find.txt"));
+
+        System.out.println("Start searching (linear search)...");
+        search.linearSearch(directoryList, findList, true);
+
+        System.out.println("Start searching (bubble sort + jump search)...");
+        List<Person> sortedBubble = sort.bubbleSort(directoryList, findList);
+        search.jumpSearch(sortedBubble, findList);
+
+        System.out.println("Start searching (quick sort + binary search)...");
+        List<Person> sortedQuick = sort.quickSort(directoryList);
+        search.binarySearch(sortedQuick, findList);
+
+        System.out.println("Start searching (hash table)...");
+        directoryTable = Files.toHash(directoryList);
+        search.hashSearch(directoryTable,findList);
+    }
+}
+
