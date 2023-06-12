f59f99ec88eb30a2c4e7633c01bf6175e5a8dc20
Alena <124808324+AlenaSmolenchuk@users.noreply.github.com>
2023-06-12 16:46:16 +0300

    Update Main.java

--- src/phonebook/Main.java
+++ src/phonebook/Main.java
@@ -2,6 +2,8 @@ package phonebook;
 
 public class Main {
     public static void main(String[] args) {
-              new Start().start();
+              
+        new Start().start();
+    
     }
 }
